0e94783ce5352e068335b308380ff9ae83486a0f
franklinlem <franklinlem@yahoo.com>
2023-08-25 14:48:07 -0300

    aulas de kotlin

--- aulas de kotlin/aula001.kt
+++ aulas de kotlin/aula001.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>){
     println("Olá Mundo!")
+    //variavel mutavel
+    var nome = "FRANKLIN L. MARTINS"
+    println(nome)
+    nome = "LEANDRO"
+    println(nome)
+    //variavel imutavel
+    val nomei = "JOSE DA SILVA"
+    println(nomei)
 }
