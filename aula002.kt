b5d6093824fde7affa62f1f15f0c289ddabe6f9e
franklinlem <franklinlem@yahoo.com>
2023-08-25 15:42:47 -0300

    aulas de kotlin

---
+++ aula002.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>){
+    /*
+    String - Textos
+    byte - numeros inteiros(-128 até 127)
+    int - Numeros inteiros (-2147483648 até 2147483647)
+    long - Numeros inteiros muito grandes
+    float - Numeros com até 7 casas decimais
+    double - Numeros com até 15 casas decimais
+    boolean - Valores Booleanos - true ou false
+    */
+    val nomeCompleto = "Franklin L. Martins"
+    println(nomeCompleto)
+    println(nomeCompleto.length)
+    var idade = 40
+    println(idade)
+    val medida = 456245675.456123
+    println(medida)
+    var dinheiro = 2.55f
+    println(dinheiro)
+    var salario = 7580.00
+    println(salario)
+    var falso = false
+    println(falso)
+}
